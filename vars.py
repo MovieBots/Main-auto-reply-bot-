c49761821f3d02362ba881709ce55de35d14fcec
owajbhai <owajshaikhrcm@gmail.com>
2025-10-05 17:38:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "N2 Auto Reply Bot")
 API_ID = 28379294
 API_HASH = "f586b601ced6eeed819b280d6a9bc8eb"
-USER_SESSION = environ.get("USER_SESSION", "")
+USER_SESSION = environ.get("USER_SESSION", "BQGxCJ4ABRoW1QEcpWaCqqyY9c6HSRyt56cCITXVnnJ4D0XgB_MV0OYqh9pH0YHHjf6D3LFLfZAoxLo1aqjfBZheo1lilXKwcdusjVWdk_SW62QQaRCiDQxr02L38hxhN2DnmzyTHG0_uummHJJCH5QYIlmn-AwQAsQYPIsZRTsAIzJOnq1xZ19cb5DXlOgsHIiWHqXV3GedN6vw45Hl8R_eAVJ-8AxxME-H9mGn_lSqOzPj-YaE-ncCe2lPlr7IZzSAltwzkyu0GMYisq0ZtRfsG-jnkVVgLzUPrQTuyxjechrxwEMA5Rbz-ZnQk3x-Ow0XhEAVJOOjhRiOFpLkMFSmcKOI5wAAAAF33rosAA")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003184409377"))
 FROM_GRP = [int(channel_id) for channel_id in environ.get('FROM_GRP', '-1002005238353 -1001232792540 -1001629066592 -1001763793679 -1001080367092').split() if re.match(r'^-?\d+$', channel_id)]
 PORT = int(environ.get("PORT", "8080"))
