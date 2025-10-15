8f47483ec5652b662da7a7b226bb7f0d04bc75c1
owajbhai <owajshaikhrcm@gmail.com>
2025-10-15 22:17:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "N2 Auto Reply Bot")
 API_ID = 29361730
 API_HASH = "9d327b43fc075d760f16bab18b4cb90e"
-USER_SESSION = environ.get("USER_SESSION", "BQGcAJsALR1kVjaDHPOdtpl7ueK01wbehgiK07TxSXu5T1bQEqrE4IQe118uRM_TRaWMyAfC-9RTSGw4dqOyhgZZnD5vkOp526oyRCuB5zHhbpH3mysE6JqKCwOXH8DlX-08b_6FrKmreOBX7P3unnEKIjInODSdoqfa9nppbCqjIURPZx9xdC1QMCpkT9tBE6DZkz6hIx5lhYMgMf46_tf8___9t4FmTw9GK0gXI9osfhK1ZQ7IFHjRCDxjORNc4Jm7WY8k4Bukc1pq3aER0KlkMvxZPoQKJIr8jxjMFZujwS56xAsJJw29L3EE8X4wLLPjYV8OEUs_SaFpK3GIYLPvT8hQnQAAAAF33rosAA")
+USER_SESSION = environ.get("USER_SESSION", "BQHABkIAbu_0JAtgStlyEbQ5rSDjsywsbQ8pVV37JG9ZrNj-DCUdv9hYng6qCh4g1gcx7IyJD6nxaD1GMQMz5xHoIquNenRmqrM_clv_CJ9elntC3tEdCQEYiLBlP2YZaq-OETXYXtOuE39KPuMGl8elg-NMDt_Nl5hHBSCqvebpFC1ovR6TZPal1716chdNV_gGA8zmm7wMtPk6y5O7k0X2qt4BTozO6MzfbDFLtucP3b_u5i_zUeJkMyDuX5tdfFkyIsN5i_UF4jIembInnOdAqHJeCwJa71YamWN6ZWj8jy_w5ZepA50l1XooBrDsf86Ir_ZsKRZ0d9e0Scq4zECUyv7BxgAAAAGad7H_AA")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003184409377"))
 FROM_GRP = [int(channel_id) for channel_id in environ.get('FROM_GRP', '-1001629066592').split() if re.match(r'^-?\d+$', channel_id)]
 PORT = int(environ.get("PORT", "8080"))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6306052652').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6886502911').split()]
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/MovieBots/Auto-Reply-Bot")
