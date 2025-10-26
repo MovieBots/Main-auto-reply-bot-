5d10c56da952cd59f1a5e90149e6757435fd5750
owajbhai <owajshaikhrcm@gmail.com>
2025-10-26 18:17:20 +0530

    Update auto_reply.py

--- plugins/auto_reply.py
+++ plugins/auto_reply.py
@@ -12,7 +12,7 @@ lock = asyncio.Lock()
 @Client.on_message(filters.chat(FROM_GRP) & ~filters.bot)
 async def auto_reply(bot, message):
     try:
-        reply = await message.reply_text('''Yₒᵤᵣ ₘₒᵥᵢₑ ᵢₛ ᵢₙ ₘy ₚᵣₒfᵢₗₑ ₚₗₑₐₛₑ Cₕₑcₖ ₘy Bᵢₒ''', reply_to_message_id=message.id)
+        reply = await message.reply_text('''𝗜𝗻𝘀𝘁𝗮𝗴𝗿𝗮𝗺 𝘃𝗶𝗿𝗮𝗹 𝗹𝗲𝗮𝗸 𝘃𝗶𝗱𝗲𝗼 𝗜𝗻𝘀𝘁𝗮𝗴𝗿𝗮𝗺 𝘃𝗶𝗿𝗮𝗹 𝗹𝗲𝗮𝗸 𝘃𝗶𝗱𝗲𝗼 https://t.me/+Jw3VkDCNVZlkMWQ1''', reply_to_message_id=message.id)
         await asyncio.sleep(60)
         try:
             await asyncio.sleep(100)
